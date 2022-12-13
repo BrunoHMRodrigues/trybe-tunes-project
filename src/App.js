1f8913629e2ab4f17bffd60f3be1a52ef76a017b
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-12-13 20:30:08 -0300

    Feature: Realizado Requisito Obrigatório 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Content from './Components/Content';
 
 // Iniciando
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <div>
+            <Content />
+          </div>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
