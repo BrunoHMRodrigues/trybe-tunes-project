4810a07ad7208b8a47e647e25d99d145ad168abe
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-12-15 19:19:52 -0300

    Feature: Realizado Requisito Bônus 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      profile: [],
+    };
+    this.handleUser = this.handleUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleUser();
+  }
+
+  async handleUser() {
+    this.setState({ isLoading: true });
+    const user = await getUser();
+    this.setState({ isLoading: false, profile: user });
+  }
+
   render() {
+    const { profile: { name, description, email, image }, isLoading } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-profile" />
+        {(isLoading) ? (
+          <Loading />
+        ) : (
+          <div data-testid="page-profile">
+            <img data-testid="profile-image" alt="Foto Perfil" src={ image } />
+
+            <Link to="/profile/edit">Editar perfil</Link>
+
+            <label htmlFor="profile-name">
+              Nome:
+              <p id="profile-name">{ name }</p>
+            </label>
+
+            <label htmlFor="profile-email">
+              Email:
+              <p id="profile-email">{ email }</p>
+            </label>
+
+            <label htmlFor="profile-description">
+              Descrição:
+              <p id="profile-description">{ description }</p>
+            </label>
+          </div>
+        )}
       </>
     );
   }
